aebe5b9e8d26cbc748ee40aefacd38ebe839c04f
Charles Chadwick <charles@mutuallyassured.com>
2025-04-10 07:29:41 -0700

    Messing with the layout. This needs to be cleaned up a lot.

--- resources/views/livewire/appointment-list.blade.php
+++ resources/views/livewire/appointment-list.blade.php
@@ -1,33 +1,53 @@
-<table class="table-list w-full">
-    <thead>
-    <tr>
-        <th>Start</th>
-        <th>End</th>
-        <th>Type</th>
-        <th>Reason</th>
-        <th>Doctor</th>
-        <th>Notes</th>
-    </tr>
-    </thead>
-    <tbody>
+<ul
+    role="list"
+    class="divide-y divide-stone-100 overflow-hidden bg-white shadow-sm ring-1 ring-stone-900/5 sm:rounded-xl"
+>
     @forelse($appointments as $appointment)
-        <tr>
-            <td>{{ $appointment->start }}</td>
-            <td>{{ $appointment->end }}</td>
-            <td>{{ $appointment->type }}</td>
-            <td>{{ $appointment->reason }}</td>
-            <td>Doc</td>
-            <td>{{ $appointment->notes }}</td>
-        </tr>
+        <li class="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-stone-50 sm:px-6">
+            <div class="flex min-w-0 gap-x-4">
 
+                <div class="min-w-0 flex-auto">
+                    <p class="text-stone-900">
+                        <flux:modal.trigger :name="'edit-appointment-'.$appointment->id">
+                            <a href="#" class="font-bold">
+                                {{ $appointment->type }} - {{ $appointment->reason }}
+                            </a>
+                        </flux:modal.trigger>
+                    </p>
+                    <p class="text-sm">
+                        {{ $appointment->timeRange }}
+                    </p>
+                    <p class="mt-1 flex text-xs/5 text-stone-500">
+                   {{ $appointment->notes }}
+                    </p>
+                </div>
+            </div>
+
+            <div class="flex shrink-0 items-center gap-x-4">
+                <div class="hidden sm:flex sm:flex-col sm:items-end">
+                    <p class="text-sm/6 text-stone-900">{{ $appointment->doctor->full_name }}</p>
+                    <p class="mt-1 text-xs/5 text-stone-500">{{ $appointment->doctor->role }}</p>
+                </div>
+                <svg
+                    class="size-5 flex-none text-stone-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    data-slot="icon"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
+            </div>
+
+        </li>
+        <flux:modal :name="'edit-appointment-'.$appointment->id">
+            <livewire:appointment-form :appointment="$appointment" />
+        </flux:modal>
     @empty
-        <tr>
-            <td
-                colspan="6"
-                class="text-center"
-            > No appointments
-            </td>
-        </tr>
+        <p>There are no appointments.</p>
     @endforelse
-    </tbody>
-</table>
+</ul>
