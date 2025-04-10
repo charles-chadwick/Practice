98cfe0af15b057eac474858fd9bc3005bfd58494
Charles Chadwick <charles@mutuallyassured.com>
2025-04-10 08:21:51 -0700

    Turned this into an array.
    I will abstract this to another enum here soon.

--- app/Enums/AppointmentStatus.php
+++ app/Enums/AppointmentStatus.php
@@ -8,4 +8,14 @@ enum AppointmentStatus: string {
     case Pending     = "Pending";
     case Rescheduled = "Rescheduled";
     case Completed   = "Completed";
+
+    public static function toArray() : array {
+        return [
+            self::Confirmed->value   => self::Confirmed->name,
+            self::Cancelled->value   => self::Cancelled->name,
+            self::Pending->value     => self::Pending->name,
+            self::Rescheduled->value => self::Rescheduled->name,
+            self::Completed->value   => self::Completed->name,
+        ];
+    }
 }
