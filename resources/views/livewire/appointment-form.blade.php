ba5d95ef65b9b0fd2bff8e6bbb79054c214e6f78
Charles Chadwick <charles@mutuallyassured.com>
2025-04-10 08:22:23 -0700

    Adding status dropdown.

--- resources/views/livewire/appointment-form.blade.php
+++ resources/views/livewire/appointment-form.blade.php
@@ -1,12 +1,20 @@
 <form wire:submit="submit">
-    <div class="grid grid-cols-2 gap-6">
+    <div class="grid grid-cols-2 gap-6 mb-4">
 
-        <flux:input
-            type="text"
+
+        <flux:select
             wire:model="status"
+            placeholder="Choose status..."
             label="Status"
-            placeholder="Status"
-        />
+        >
+            @foreach(\App\Enums\AppointmentStatus::toArray() as $key => $value)
+                <flux:select.option
+                    wire:key="{{ $key }}"
+                    value="{{ $key }}"
+                >{{ $value }}</flux:select.option>
+            @endforeach
+
+        </flux:select>
 
         <flux:input
             type="text"
@@ -14,34 +22,45 @@
             label="Type"
             placeholder="Type"
         />
+    </div>
 
+    <div class="grid grid-cols-3 gap-6 mb-4">
         <flux:input
             type="date"
-            wire:model="start"
+            wire:model="start_date"
             label="Start Date"
             placeholder="Start Date"
         />
-
         <flux:input
-            type="date"
-            wire:model="end"
-            label="End Date"
-            placeholder="End Date"
+            type="time"
+            wire:model="start_time"
+            label="Time"
+            placeholder="Time"
         />
-
-        <flux:textarea
+        <flux:input
+            type="number"
+            wire:model="duration"
+            label="Duration"
+            placeholder="Duration"
+        />
+    </div>
+    <div class="grid grid-cols-1 gap-6 mb-4">
+        <flux:input
+            type="text"
             wire:model="reason"
-            label="Description"
-            placeholder="Description"
+            label="Reason For Visit"
+            placeholder="Reason"
         />
+    </div>
 
+    <div class="grid grid-cols-1 gap-6 mb-4">
         <flux:textarea
             wire:model="notes"
             label="Notes"
             placeholder="Notes"
         />
-
     </div>
+
     <div class="text-center gap-4 pt-6 ">
         <button
             type="submit"
