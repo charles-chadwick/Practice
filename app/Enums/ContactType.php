7464d018a437a4f06a8b6c2d64f3ca133f0ee311
Charles Chadwick <charles@mutuallyassured.com>
2025-04-10 12:26:15 -0700

    Added toArray

--- app/Enums/ContactType.php
+++ app/Enums/ContactType.php
@@ -7,4 +7,10 @@ enum ContactType: string {
     case Home = 'Home';
     case Work = 'Work';
 
+    public static function toArray() : array {
+        return [
+            self::Home->value => self::Home->name,
+            self::Work->value => self::Work->name
+        ];
+    }
 }
